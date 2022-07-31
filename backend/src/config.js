a5a307af96e959790ee4b7371490aee46e2ec443
Sara <savethekitties.nft@gmail.com>
2022-07-31 15:45:59 -0500

    changes config

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,7 +58,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-07-29T05:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-07-29T05:30:48+00:00"; // Optional. Eg: 2022-0 2-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "null"; // Optional. Eg: 2022-0 2-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xe64A90084d57948fBA1cBe9c274847db6306816c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
